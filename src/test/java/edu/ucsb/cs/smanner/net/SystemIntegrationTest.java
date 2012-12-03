769869a97f68d3abff5e0b1dec051a6a31ddf40a
Alexander Pucher <dev@alexpucher.com>
2012-12-03 14:49:08 -0800

    system integration with locking and clients
    Signed-off-by: Alexander Pucher <dev@alexpucher.com>

--- src/test/java/edu/ucsb/cs/smanner/net/SystemIntegrationTest.java
+++ src/test/java/edu/ucsb/cs/smanner/net/SystemIntegrationTest.java
@@ -19,6 +19,7 @@ import edu.ucsb.cs.smanner.protocol.paxos.ProposalListener;
 import edu.ucsb.cs.smanner.protocol.tpc.TransactionExecutor;
 import edu.ucsb.cs.smanner.protocol.tpc.TwoPhaseCommitCoordinator;
 import edu.ucsb.cs.smanner.protocol.tpc.TwoPhaseCommitParticipant;
+import edu.ucsb.cs.smanner.store.LockManager;
 
 public class SystemIntegrationTest {
 	private static Logger log = LoggerFactory.getLogger(SystemIntegrationTest.class);
@@ -32,9 +33,11 @@ public class SystemIntegrationTest {
 
 	PaxosLeader leaderA;
 	PaxosFollower followerA;
+	LockManager lockA;
 
 	PaxosLeader leaderB;
 	PaxosFollower followerB;
+	LockManager lockB;
 
 	TwoPhaseCommitCoordinator coordinator;
 	TwoPhaseCommitParticipant participantA;
@@ -65,15 +68,27 @@ public class SystemIntegrationTest {
 				log.debug("notified Paxos Group B");
 				if(operation instanceof PaxosPrepareOperation) {
 					// prepare
-					participantA.acceptPrepare(((PaxosPrepareOperation) operation).transactionId);
+					try {
+						lockA.lock(operation.getId());
+						participantA.acceptPrepare(((PaxosPrepareOperation) operation).transactionId);
+					} catch(Exception e) {
+						participantA.declinePrepare(((PaxosPrepareOperation) operation).transactionId);
+					}
 				} else if(operation instanceof PaxosCommitOperation) {
 					// commit
+					sleep(200);
+					lockA.unlock();
 					participantA.commitTransaction(((PaxosCommitOperation) operation).transactionId);
+				}  else if(operation instanceof PaxosAbortOperation) {
+					// abort
+					lockA.unlock();
+					participantA.abortTransaction(((PaxosAbortOperation) operation).transactionId);
 				} else {
 					log.error("unknown operation {}", operation.getId());
 				}
 			}
 		});
+		lockA = new LockManager();
 		
 		// Paxos Group B
 		leaderB = (PaxosLeader) context2.getBean("protocolBL");
@@ -84,15 +99,27 @@ public class SystemIntegrationTest {
 				log.debug("notified Paxos Group B");
 				if(operation instanceof PaxosPrepareOperation) {
 					// prepare
-					participantB.acceptPrepare(((PaxosPrepareOperation) operation).transactionId);
+					try {
+						lockB.lock(operation.getId());
+						participantB.acceptPrepare(((PaxosPrepareOperation) operation).transactionId);
+					} catch(Exception e) {
+						participantB.declinePrepare(((PaxosPrepareOperation) operation).transactionId);
+					}
 				} else if(operation instanceof PaxosCommitOperation) {
 					// commit
+					sleep(200);
+					lockB.unlock();
 					participantB.commitTransaction(((PaxosCommitOperation) operation).transactionId);
+				}  else if(operation instanceof PaxosAbortOperation) {
+					// abort
+					lockB.unlock();
+					participantB.abortTransaction(((PaxosAbortOperation) operation).transactionId);
 				} else {
 					log.error("unknown operation {}", operation.getId());
 				}
 			}
 		});
+		lockB = new LockManager();
 		
 		// Two Phase Commit
 		coordinator = (TwoPhaseCommitCoordinator) context2.getBean("protocolTpcL");
@@ -108,7 +135,7 @@ public class SystemIntegrationTest {
 			}
 			@Override
 			public void abort(long id, Operation operation) {
-				// left blank	
+				leaderA.addProposal(new PaxosAbortOperation(operation.getId(), id));
 			}
 		});
 		
@@ -123,7 +150,7 @@ public class SystemIntegrationTest {
 			}
 			@Override
 			public void abort(long id, Operation operation) {
-				// left blank	
+				leaderB.addProposal(new PaxosAbortOperation(operation.getId(), id));
 			}
 		});
 		
@@ -193,7 +220,7 @@ public class SystemIntegrationTest {
 	}
 	
 	@Test(timeout = 1000)
-	public void testMultiClient() throws Exception {
+	public void testMultiClientAbort() throws Exception {
 		Map<String, Operation> operations = new HashMap<String, Operation>();
 		operations.put("tpcA", new NullOperation("A"));
 		operations.put("tpcB", new NullOperation("B"));
@@ -202,7 +229,7 @@ public class SystemIntegrationTest {
 		long id2 = client2.create(operations);
 		
 		while(client1.getState(id1) != TransactionState.COMMITTED ||
-			  client2.getState(id2) != TransactionState.COMMITTED) {
+			  client2.getState(id2) != TransactionState.ABORTED) {
 			Thread.sleep(100);
 		}
 	}
@@ -244,4 +271,29 @@ public class SystemIntegrationTest {
 		}
 	}
 
+	@SuppressWarnings("serial")
+	static class PaxosAbortOperation extends Operation {
+		final long transactionId;
+
+		public PaxosAbortOperation(String id, long transactionId) {
+			super(id);
+			this.transactionId = transactionId;
+		}
+
+		public long getTransactionId() {
+			return transactionId;
+		}
+	}
+	
+	static void sleep(long timeMillis) {
+		long start = System.currentTimeMillis();
+		while(System.currentTimeMillis() < start + timeMillis) {
+			try {
+				Thread.sleep(timeMillis);
+			} catch (InterruptedException e) {
+				// ignore
+			}
+		}
+	}
+	
 }
