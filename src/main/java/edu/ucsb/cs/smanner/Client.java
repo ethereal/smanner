db1dd18a27f49d062fc3e20ae34ce87babb7f512
Alexander Pucher <dev@alexpucher.com>
2012-12-04 17:29:00 -0800

    multi-request client
    Signed-off-by: Alexander Pucher <dev@alexpucher.com>

--- src/main/java/edu/ucsb/cs/smanner/Client.java
+++ src/main/java/edu/ucsb/cs/smanner/Client.java
@@ -34,20 +34,34 @@ public class Client {
 	}
 	
 	public void execute(String args[]) {
-		String opId = UUID.randomUUID().toString();
-		Map<String, Operation> operations = new HashMap<String, Operation>();
-		
 		if("read".equals(args[0])) {
+			String opId = UUID.randomUUID().toString();
+			Map<String, Operation> operations = new HashMap<String, Operation>();
 			operations.put("tpcA", new ReadOperation(opId));
 			operations.put("tpcB", new ReadOperation(opId));
+			runTransaction(operations);
 		} else if("write".equals(args[0])) {
+			String opId = UUID.randomUUID().toString();
+			Map<String, Operation> operations = new HashMap<String, Operation>();
 			operations.put("tpcA", new WriteOperation(opId, args[1]));
 			operations.put("tpcB", new WriteOperation(opId, args[2]));
+			runTransaction(operations);
+		}  else if("multi".equals(args[0])) {
+			int runs = Integer.valueOf(args[1]);
+			for(int i=0; i<runs; i++) {
+				String opId = UUID.randomUUID().toString();
+				Map<String, Operation> operations = new HashMap<String, Operation>();
+				operations.put("tpcA", new WriteOperation(opId, args[2] + i));
+				operations.put("tpcB", new WriteOperation(opId, args[3] + i));
+				runTransaction(operations);
+			}
 		} else {
 			log.error("Unknown command");
 			return;
 		}
-
+	}
+	
+	void runTransaction(Map<String, Operation> operations) {
 		long tid = endpoint.create(operations);
 		
 		try {
@@ -55,18 +69,28 @@ public class Client {
 			while(state != TransactionState.COMMITTED) {
 				// retry externally
 				if(state == TransactionState.ABORTED) {
-					log.debug("transaction {} failed. Retrying.", tid);
+					log.info("transaction {} failed. Retrying.", tid);
 					tid = endpoint.create(operations);
 				}
 				
-				Thread.sleep(10);
+				Thread.sleep(100);
 				state = endpoint.getState(tid);
 			}
 
 			log.info("transaction {} completed with state {}", tid, state);
-			if("read".equals(args[0]) && state == TransactionState.COMMITTED) {
-				log.info("result A:\n{}", ((ReadOperationResult)endpoint.getResult(tid).get("tpcA")).getResult());
-				log.info("result B:\n{}", ((ReadOperationResult)endpoint.getResult(tid).get("tpcB")).getResult());
+			if(state == TransactionState.COMMITTED && endpoint.getResult(tid).get("tpcA") instanceof ReadOperationResult) {
+				String a[] = ((ReadOperationResult)endpoint.getResult(tid).get("tpcA")).getResult().split("\n");
+				String b[] = ((ReadOperationResult)endpoint.getResult(tid).get("tpcB")).getResult().split("\n");
+				
+				StringBuilder sb = new StringBuilder();
+				for(int i=0; i<a.length; i++) {
+					sb.append(a[i]);
+					sb.append('\t');
+					sb.append(b[i]);
+					sb.append('\n');
+				}
+				
+				log.info("result:\n{}", sb);
 			}
 		} catch (InterruptedException e) {
 			log.warn("Interrupted. Exisiting.");
@@ -75,7 +99,7 @@ public class Client {
 	
 	public void setUp() throws Exception {
 		log.trace("Client::setUp()");
-		context = NodeTool.createContext("/META-INF/spring/client.xml");
+		context = NodeTool.createContext("/META-INF/spring/awsclient.xml");
 		endpoint = (TransactionEndpoint)context.getBean("smanner");
 	}
 
